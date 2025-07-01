6a7cd0b376c6c574772c9762d13fce769c100b87
dalilamostukova <dalilya2417@icloud.com>
2025-07-01 21:29:25 +0300

    температура - 3 острова - 4

---
+++ src/main/java/kanan/Task3.java
@@ -0,0 +1,30 @@
+package kanan;
+
+import java.util.Stack;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int[] temperatures = {17, 16, 19, 15, 13, 18, 20};
+        int[] result = dailyTemperatures(temperatures);
+
+        System.out.print("Количество суток до более тёплого дня:\n");
+        for (int num : result) {
+            System.out.print(num + "    ");
+        }
+    }
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
+                result[prevIndex] = i - prevIndex;
+            }
+            stack.push(i);
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/kanan/Task4.java
@@ -0,0 +1,54 @@
+package kanan;
+
+public class Task4 {
+    static class Islands {
+        public int howMuchlands(char[][] matrix) {
+            if (matrix == null || matrix.length == 0)
+                return 0;
+
+            int numIslands = 0;
+
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    if (matrix[i][j] == '1') {
+                        numIslands++;
+                        markIsland(matrix, i, j);
+                    }
+                }
+            }
+            return numIslands;
+        }
+
+        private void markIsland(char[][] matrix, int i, int j) {
+            if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[i].length || matrix[i][j] == '0')
+                return;
+
+            matrix[i][j] = '0';
+
+            markIsland(matrix, i, j + 1);
+            markIsland(matrix, i + 1, j);
+            markIsland(matrix, i, j - 1);
+            markIsland(matrix, i - 1, j);
+        }
+    }
+
+    public static void main(String[] args) {
+        Islands islandsCounter = new Islands();
+
+        char[][] grid1 = {
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+        System.out.println("Количество островов: " + islandsCounter.howMuchlands(grid1));
+
+        char[][] grid2 = {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+        System.out.println("Количество островов: " + islandsCounter.howMuchlands(grid2));
+    }
+}
